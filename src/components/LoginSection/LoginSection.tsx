f4ea49ce56a9c33b542604f0230ce255fee285d8
EduardoLCavalcante <eduardocavalcante131@gmail.com>
2025-01-10 13:43:52 -0300

    Visualizacao de senha

--- src/components/LoginSection/LoginSection.tsx
+++ src/components/LoginSection/LoginSection.tsx
@@ -1,6 +1,8 @@
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
+import { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
 
 interface LoginSectionProps{
     email: string,
@@ -12,6 +14,7 @@ interface LoginSectionProps{
 }
 
 const LoginSection = (props: LoginSectionProps) => {
+    const [showPassword, setShowPassword] = useState(false);
   return (
     <>
         <div className="space-y-2">
@@ -25,17 +28,26 @@ const LoginSection = (props: LoginSectionProps) => {
             className="border-green-300 focus:border-green-500 focus:ring-green-500"
         />
     </div>
-    <div className="space-y-2">
+    <div className="space-y-2 relative">
         <Label htmlFor="password">Senha</Label>
-        <Input
-        id="password"
-        type="password"
-        value={props.password}
-        onChange={(e) => props.setPassword(e.target.value)}
-        required
-        className="border-green-300 focus:border-green-500 focus:ring-green-500"
-        />
-    </div>
+        <div className="relative">
+          <Input
+            id="password"
+            type={showPassword ? "text" : "password"}
+            value={props.password}
+            onChange={(e) => props.setPassword(e.target.value)}
+            required
+            className="border-green-300 focus:border-green-500 focus:ring-green-500 pr-10"
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="absolute inset-y-0 right-2 flex items-center text-sm text-gray-500 hover:text-gray-700 focus:outline-none"
+          >
+            {showPassword ? <Eye/> : <EyeOff/>}
+          </button>
+        </div>
+      </div>
     {/* Exibe a mensagem de erro se houver */}
     {props.loginError && (
         <p className="text-red-500 text-sm">
--- src/components/SignUpSection/SignUpSection.tsx
+++ src/components/SignUpSection/SignUpSection.tsx
@@ -2,6 +2,8 @@ import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { CheckboxWithText } from '../Checkbox/checkboxText'
 import { Button } from '../ui/button'
+import { Eye, EyeOff } from 'lucide-react'
+import { useState } from 'react'
 
 interface SignUpProps{
     email : string,
@@ -16,6 +18,8 @@ interface SignUpProps{
 }
 
 const SignUpSection = (props: SignUpProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <>
     <div className="space-y-2">
@@ -42,16 +46,25 @@ const SignUpSection = (props: SignUpProps) => {
   </div>
 
   <div className="space-y-2">
-    <Label htmlFor="newPassword">Senha</Label>
-    <Input
-      id="newPassword"
-      type="password"
-      value={props.password}
-      onChange={(e) => props.setPassword(e.target.value)}
-      required
-      className="border-green-300 focus:border-green-500 focus:ring-green-500"
-    />
-  </div>
+      <Label htmlFor="newPassword">Senha</Label>
+      <div className="relative">
+      <Input
+        id="newPassword"
+        type={showPassword ? "text" : "password"}
+        value={props.password}
+        onChange={(e) => props.setPassword(e.target.value)}
+        required
+        className="border-green-300 focus:border-green-500 focus:ring-green-500"
+      />
+      <button
+        type="button"
+        onClick={() => setShowPassword((prev) => !prev)}
+        className="absolute inset-y-0 right-2 flex items-center text-sm text-gray-500 hover:text-gray-700 focus:outline-none"
+        >
+        {showPassword ? <Eye/> : <EyeOff/>}
+      </button>
+      </div>
+    </div>
 
   <CheckboxWithText
     handleCheckboxChange={props.handleCheckboxChange}
